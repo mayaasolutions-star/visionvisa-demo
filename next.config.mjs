878355acca155b58664736506fa64f9e1f02f12b
mayaasolutions@4 <mayaasolutions@gmail.com>
2026-08-12 02:15:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: false,
 
   output: 'export',
 
   basePath: '/visionvisa-demo',
+  assetPrefix: '/visionvisa-demo/',
 
   trailingSlash: true,
 
